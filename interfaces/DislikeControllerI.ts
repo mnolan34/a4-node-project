24dc6050273d2d1b5f50a61f36e36c6a86c28586
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-02 11:09:19 -0400

    Implemented ControllerI, Controller of Dislikes, next is need to fix stats

---
+++ interfaces/DislikeControllerI.ts
@@ -0,0 +1,7 @@
+import {Request, Response} from "express";
+
+export default interface LikeControllerI {
+    findAllUsersThatDislikedTuit (req: Request, res: Response): void;
+    findAllTuitsDislikedByUser (req: Request, res: Response): void;
+    userTogglesTuitDislikes (req: Request, res: Response): void;
+};
